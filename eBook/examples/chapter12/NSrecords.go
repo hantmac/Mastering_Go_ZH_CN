de28974c1fd0af3167e42bed53a1eea8b0e1f16d
glbrtchen <glbrt.chen@gmail.com>
2019-02-01 06:31:44 +0800

    增加第十二章第六小节

---
+++ eBook/examples/chapter12/NSrecords.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Need a domain name!")
+		return
+	}
+
+	domain := arguments[1]
+	NSs, err := net.LookupNS(domain)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, NS := range NSs {
+		fmt.Println(NS.Host)
+	}
+}
