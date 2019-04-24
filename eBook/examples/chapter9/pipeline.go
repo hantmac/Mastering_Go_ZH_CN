5948dcf72282a5ae7050d2680a14e83ba80dca50
D119 <contact@moonbear.cn>
2019-04-24 17:36:15 +0800

    9.5

---
+++ eBook/examples/chapter9/pipeline.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+var CLOSEA = false
+var DATA = make(map[int]bool)
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func first(min, max int, out chan<- int) {
+	for {
+		if CLOSEA {
+			close(out)
+			return
+		}
+		out <- random(min, max)
+	}
+}
+
+func second(out chan<- int, in <-chan int) {
+	for x := range in {
+		fmt.Print(x, " ")
+		_, ok := DATA[x]
+		if ok {
+			CLOSEA = true
+		} else {
+			DATA[x] = true
+			out <- x
+		}
+	}
+	fmt.Println()
+	close(out)
+}
+
+func third(in <-chan int) {
+	var sum int
+	sum = 0
+	for x2 := range in {
+		sum = sum + x2
+	}
+	fmt.Println("The sum of the random numbers is %d\n", sum)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Need two integer paramters!")
+		os.Exit(1)
+	}
+	n1, _ := strconv.Atoi(os.Args[1])
+	n2, _ := strconv.Atoi(os.Args[2])
+
+	if n1 > n2 {
+		fmt.Printf("%d should be smaller than %d\n", n1, n2)
+		return
+	}
+	rand.Seed(time.Now().UnixNano())
+	A := make(chan int)
+	B := make(chan int)
+
+	go first(n1, n2, A)
+	go second(B, A)
+	third(B)
+}
