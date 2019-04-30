34bec8f90c567d41c1351e71682bbd70eda5929e
kasheemlew <417879751@qq.com>
2019-04-30 16:36:04 +0800

    05.3 05.3.1 05.3.2

---
+++ eBook/examples/chapter5/binTree.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Tree struct {
+	Left  *Tree
+	Value int
+	Right *Tree
+}
+
+func traverse(t *Tree) {
+	if t == nil {
+		return
+	}
+	traverse(t.Left)
+	fmt.Println(t.Value, " ")
+	traverse(t.Right)
+}
+
+func create(n int) *Tree {
+	var t *Tree
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < 2*n; i++ {
+		temp := rand.Intn(n * 2)
+		t = insert(t, temp)
+	}
+	return t
+}
+
+func insert(t *Tree, v int) *Tree {
+	if t == nil {
+		return &Tree{nil, v, nil}
+	}
+	if v == t.Value {
+		return t
+	}
+	if v < t.Value {
+		t.Left = insert(t.Left, v)
+		return t
+	}
+	t.Right = insert(t.Right, v)
+	return t
+}
+
+func main() {
+	tree := create(10)
+	fmt.Println("The value of the root of the tree is", tree.Value)
+	traverse(tree)
+	fmt.Println()
+	tree = insert(tree, -10)
+	tree = insert(tree, -2)
+	traverse(tree)
+	fmt.Println()
+	fmt.Println("The value of the root of the tree is", tree.Value)
+}
